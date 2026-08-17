cb027115cf85ae8ab94f8ecd15371da5cfb11230
ojdevsent-lgtm <ojdevs.ent@gmail.com>
2026-08-17 11:23:32 +0100

    Migrate leaderboard service to Firebase Functions

--- js/services/leaderboard.service.js
+++ js/services/leaderboard.service.js
@@ -1,16 +1,11 @@
-import { supabase } from "../supabase.js";
+import { functions, httpsCallable } from "../firebase.js";
 
 const TIMEOUT_MS = 12000;
+function withTimeout(promise) { return Promise.race([promise, new Promise((_, reject) => setTimeout(() => reject(new Error("The leaderboard took too long to respond.")), TIMEOUT_MS))]); }
 
-function withTimeout(promise) {
-  return Promise.race([
-    promise,
-    new Promise((_, reject) => setTimeout(() => reject(new Error("The leaderboard took too long to respond.")), TIMEOUT_MS))
-  ]);
-}
+const getLeaderboardCallable = httpsCallable(functions, "getLeaderboard");
 
 export async function getLeaderboard(mode = "overall") {
-  const { data, error } = await withTimeout(supabase.rpc("get_leaderboard", { p_mode: mode }));
-  if (error) throw error;
-  return data ?? [];
+  const result = await withTimeout(getLeaderboardCallable({ mode }));
+  return result?.data ?? [];
 }
