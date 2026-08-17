126212e17b31b24c188901beea4a3521399c2116
ojdevsent-lgtm <ojdevs.ent@gmail.com>
2026-08-17 11:23:28 +0100

    Migrate quiz service to Firebase Functions

--- js/services/quiz.service.js
+++ js/services/quiz.service.js
@@ -1,22 +1,17 @@
-import { supabase } from "../supabase.js";
+import { functions, httpsCallable } from "../firebase.js";
 
 const TIMEOUT_MS = 12000;
+function withTimeout(promise, ms = TIMEOUT_MS) { return Promise.race([promise, new Promise((_, reject) => setTimeout(() => reject(new Error("The quiz service timed out. Please try again.")), ms))]); }
 
-function withTimeout(promise, ms = TIMEOUT_MS) {
-  return Promise.race([
-    promise,
-    new Promise((_, reject) => setTimeout(() => reject(new Error("The quiz service timed out. Please try again.")), ms))
-  ]);
-}
+const getDailyQuizCallable = httpsCallable(functions, "getDailyQuiz");
+const submitDailyQuizCallable = httpsCallable(functions, "submitDailyQuiz");
 
 export async function getDailyQuiz() {
-  const { data, error } = await withTimeout(supabase.rpc("get_daily_quiz"));
-  if (error) throw error;
-  return data ?? { completed: false, questions: [] };
+  const result = await withTimeout(getDailyQuizCallable());
+  return result?.data ?? { completed: false, questions: [] };
 }
 
 export async function submitDailyQuiz(answers) {
-  const { data, error } = await withTimeout(supabase.rpc("submit_daily_quiz", { p_answers: answers }));
-  if (error) throw error;
-  return data;
+  const result = await withTimeout(submitDailyQuizCallable({ answers }));
+  return result?.data;
 }
